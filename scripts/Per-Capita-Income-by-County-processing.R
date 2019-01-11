b0bc096ba5ee2bd2c88210b467d0edff456bc42e
Zorawar Moolenaar <zorawar87@gmail.com>
2019-01-11 15:35:49 -0500

    updated for 2017

--- scripts/Per-Capita-Income-by-County-processing.R
+++ scripts/Per-Capita-Income-by-County-processing.R
@@ -1,8 +1,9 @@
 library(dplyr)
 library(acs)
-library(datapkg)
 library(tidyr)
 source('./scripts/acsHelpers.R')
+source('./scripts/datapkg_read.R')
+source('./scripts/utils.R')
 
 ##################################################################
 #
@@ -20,7 +21,7 @@ x2016_files <- dir(path_to_raw_data, recursive=T, pattern = "ACS")
 
 #Get state data
 geography=geo.make(state=09)
-yearlist=c(2009:2016)
+yearlist=c(2009:2017)
 span = 5
 col.names="pretty" 
 key="ed0e58d2538fb239f51e01643745e83f380582d7"
@@ -143,71 +144,13 @@ for (i in seq_along(yearlist)) {
   }
   county_data <- rbind(county_data, inter_data)
 }
-# 
-# #process 2016 data
-# x2016_data <- data.frame(stringsAsFactors = FALSE)
-# for (i in 1:length(x2016_files)) {
-#   data <- read.acs(paste0(path_to_raw_data, "/", x2016_files[i]), endyear=2016, span=5)
-#   race <- races[i]
-#   year <- data@endyear
-#   year <- paste(year-4, year, sep="-")
-#   geo <- data@geography
-#   geo$Geography <- gsub(", Connecticut", "", geo$Geography)
-#   geo$Id2 <- gsub("^", "0", geo$Id2)
-#   geo$Id <- NULL
-#   pc_income <- acsSum(data, 1, "Per Capita Income")
-#   estimates <- data.table(
-#             geo, 
-#             estimate(pc_income),
-#             year,
-#             race,
-#             "Number",
-#             "Median Household Income"
-#         )
-#     moes <- data.table(
-#             geo,
-#             standard.error(pc_income) * 1.645,
-#             year,
-#             race,
-#             "Number",
-#             "Margins of Error"
-#         )
-#     numberNames <- c(
-#             "County", "FIPS",
-#             "Total",
-#             "Year",
-#             "Race/Ethnicity",
-#             "Measure Type",
-#             "Variable"
-#          )
-#     setnames(estimates, numberNames)
-#     setnames(moes, numberNames)
-#     numbersData.melt <- melt(
-#             rbind(estimates, moes),
-#             id.vars=c("County", "FIPS", "Year", "Measure Type", "Variable", "Race/Ethnicity"),
-#             variable.name="Per Capita Income",
-#             variable.factor = F,
-#             value.name="Value",
-#             value.factor = F
-#     )
-#     x2016_data <- rbind(x2016_data, numbersData.melt)  
-# }
 
 pcap_income <- rbind(county_data, state_data)
-
 pcap_income$`Per Capita Income` <- NULL
-
 pcap_income$Value <- round(pcap_income$Value, 2)
 
 pcap_income <- pcap_income %>% 
   select(County, FIPS, Year, `Race/Ethnicity`, `Measure Type`, Variable, Value) %>% 
   arrange(County, Year, `Race/Ethnicity`, `Measure Type`)
 
-write.table (
-  pcap_income,
-  file.path(getwd(), "data", "per_capita_income_county_2016.csv"),
-  sep = ",",
-  row.names = F,
-  na = "-9999"
-)
-
+WriteDFToTable(pcap_income, "per_capita_income_county_2017.csv")
